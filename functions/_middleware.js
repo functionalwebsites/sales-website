c73538d9fe63100a5a1ecc13bf5669a206f5f9f5
𝕱𝖚𝖓𝖈𝖙𝖎𝖔𝖓𝖆𝖑 𝖂𝖊𝖇𝖘𝖎𝖙𝖊𝖘 <askcpr@gmail.com>
2026-04-29 00:10:37 -0700

    middlewarefix

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,13 +2,9 @@ const BUILDER_HOST = 'build.functionalwebsites.com';
 
 export async function onRequest(context) {
   const url = new URL(context.request.url);
-  const isBuilderEntry = url.pathname === '/site-builder'
-    || url.pathname === '/site-builder/'
-    || url.pathname === '/site-builder/index.html';
 
-  if ((url.hostname === BUILDER_HOST && (url.pathname === '/' || url.pathname === '/index.html' || isBuilderEntry))
-    || isBuilderEntry) {
-    const builderUrl = new URL('/site-builder/index.html', url);
+  if (url.hostname === BUILDER_HOST && (url.pathname === '/' || url.pathname === '/index.html')) {
+    const builderUrl = new URL('/site-builder/', url);
     return context.env.ASSETS.fetch(new Request(builderUrl, context.request));
   }
 
